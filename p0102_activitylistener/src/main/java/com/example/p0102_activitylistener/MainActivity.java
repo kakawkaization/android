ae92f9771a03f4f3db8a176e6eae0a632abac708
kakawkaization <kakawkaization@gmail.com>
2018-12-22 22:54:31 +0300

    lesson 11

---
+++ p0102_activitylistener/src/main/java/com/example/p0102_activitylistener/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.p0102_activitylistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnOk:
+                tvOut.setText("Pressed button OK");
+                break;
+            case R.id.btnCancel:
+                tvOut.setText("Pressed button Cancel");
+                break;
+        }
+    }
+
+
+    public void onClickStart(View v) {
+        tvOut.setText("Pressed button Start");
+    }
+}
